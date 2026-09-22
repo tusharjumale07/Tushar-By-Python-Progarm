d9cc005fc38379c57e8a8698d38fc67a6aba1791
Tushar Jumale <tusharjumale07@gmail.com>
2026-09-22 21:01:41 +0530

    refactor: clean up code and remove commented-out sections

--- Day 12/theory.py
+++ Day 12/theory.py
@@ -1,11 +1,34 @@
 #scope
 
-enimies = 1
+#local scope
+# def increase_enimies():
+#     enimies = 2
+#     print(f"enimies inside function : {enimies}")
+# increase_enimies()
+# print(f"enimies inside function : {enimies}")
 
-def increase_enimies():
-    enimies = 2
-    print(f"enimies inside function : {enimies}")
+#global scope 
+# enimies = 2
+# def increase_enimies():
+#     print(f"enimies inside function : {enimies}")
+# increase_enimies()
+# print(f"enimies inside function : {enimies}")
 
-increase_enimies()
 
-print(f"enimies inside function : {enimies}")
+"""loops and if else statements does error like this , this only happens"""
+# if 3>2:
+#     a = 10
+
+# print(a)
+
+#global constants
+"""you should write global constants in uppercase letters """
+
+
+
+
+
+
+
+
+
